4bf29b5f85c76e8fcfe86b762f9fd328a6ca7681
user-kylelee <user.kylelee@gmail.com>
2023-02-01 16:33:20 -0500

    Finish routes for navbar

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import Registry from "./pages/Registry/Registry";
+import Shop from "./pages/Shop/Shop";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
 const Layout = () => {
   return (
     <div className="app">
@@ -29,6 +33,22 @@ const router = createBrowserRouter([
       {
         path: "/signup",
         element: <Signup />,
+      },
+      {
+        path: "/registry",
+        element: <Registry />,
+      },
+      {
+        path: "/shop",
+        element: <Shop />,
+      },
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/contact",
+        element: <Contact />,
       },
     ],
   },
